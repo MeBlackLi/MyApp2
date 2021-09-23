0ee119d78f01778c75850dbb81f7c867e71e273d
BlackLee <blacklee1314@outlook.com>
2021-09-23 23:30:13 +0800

    Add new BlackApp with .kv file .
    Signed-off-by: BlackLee <blacklee1314@outlook.com>

--- main.py
+++ main.py
@@ -6,16 +6,35 @@ An application can be built if you return a widget on build(), or if you set
 self.root.
 '''
 
-# from jnius import autoclass
-# from sys import addaudithook
+import time
+from os import times
+from enum import Flag
 from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 from kivy.uix.gridlayout import GridLayout
 from kivy.app import App
 import kivy
-from plyer import vibrator
-from plyer import gps
+from plyer import flash
+from plyer import brightness
+from plyer import temperature
+from plyer import storagepath
+from plyer import spatialorientation
+from plyer import proximity
+from plyer import processors
+from plyer import irblaster
+from plyer import light
+from plyer import gyroscope
+from plyer import gravity
+from plyer import compass
+from plyer import bluetooth
+from plyer import barometer
+from plyer import accelerometer
+from plyer import vibrator, gps
+
+
+# from jnius import autoclass
+# from sys import addaudithook
 kivy.require('2.0.0')
 
 
@@ -31,63 +50,102 @@ kivy.require('2.0.0')
 # Context = autoclass('android.content.Context')
 # vibrator = activity.getSystemService(Context.VIBRATOR_SERVICE)
 
+# from android.permissions import request_permissions, Permission
+# request_permissions([Permission.WRITE_EXTERNAL_STORAGE])
 
-class Tools(GridLayout):
+class MainWidget(GridLayout):
     def __init__(self, **kwargs):
-        super(Tools, self).__init__(**kwargs)
-        self.cols = 2
-        self.add_widget(Label(text='Longitude'))
-        self.longitude = TextInput(multiline=False)
-        self.add_widget(self.longitude)
-        self.add_widget(Label(text='Latitude'))
-        self.latitude = TextInput(multiline=False)
-        self.add_widget(self.latitude)
-        self.curLon = Label(text='Cur Lon:')
-        self.add_widget(self.curLon)
-        self.curLonPos = Label(text='XX.XX')
-        self.add_widget(self.curLonPos)
-        self.curLat = Label(text='Cur Lat:')
-        self.add_widget(self.curLat)
-        self.curLatPos = Label(text='XX.XX')
-        self.add_widget(self.curLatPos)
-        self.haveVibrator = Label()
-        self.add_widget(self.haveVibrator)
-        self.startGps = Button(text='Gps Play')
-        self.startGps.bind(on_press=self.playGps)
-        self.add_widget(self.startGps)
-        self.playGpsState = False
-        self.haveVibrator.text = str(vibrator.exists())
+        super(MainWidget, self).__init__(**kwargs)
         self.existVibrator = vibrator.exists()
+        self.cols = 2
+        self.playState = False
+        self.flashState = False
         gps.configure(on_location=self.gps_update)
 
     def gps_update(self, **kwargs):
-        self.startGps.text = '\n'.join(
+        self.ids['lat_set'].text = '\n'.join(
             [f'{key}:{value}' for key, value in kwargs.items()])
         # self.startGps.text = str(kwargs).split(',').join('\n')
-        self.curLatPos.text = "{lat}".format(**kwargs)
-        self.curLonPos.text = "{lon}".format(**kwargs)
+        self.ids['lat_value'].text = "{lat}".format(**kwargs)
+        self.ids['lon_value'].text = "{lon}".format(**kwargs)
 
-    def playGps(self, instance):
+    def flash_switch(self, instance):
+        if(self.flashState):
+            self.flashState = False
+            self.ids['flash'].text = 'Flash Off'
+            flash.off()
+        else:
+            self.flashState = True
+            self.ids['flash'].text = "Flash On"
+            flash.on()
+
+    def play_callback(self, instance):
         if(self.existVibrator):
             vibrator.vibrate(0.1)
         # jnius function
         # vibrator.vibrate(10000)  # the argument is in milliseconds
 
-        if(self.playGpsState):
+        if(self.playState):
+            self.ids['btn_getGps'].text = 'Play Stop'
+            accelerometer.disable()
+            barometer.disable()
+            compass.disable()
+            gravity.disable()
+            gyroscope.disable()
+            light.disable()
+            # proximity.disable()
+            temperature.disable()
+            spatialorientation.disable_listener()
             gps.stop()
-            self.playGpsState = False
-            self.startGps.text = 'Play Stop'
+            self.playState = False
         else:
+            accelerometer.enable()
+            barometer.enable()
+            compass.enable()
+            gravity.enable()
+            gyroscope.enable()
+            light.enable()
+            # proximity.enable()
+            temperature.enable()
+            spatialorientation.enable_listener()
             gps.start()
-            self.playGpsState = True
-            self.startGps.text = 'Play Start'
+            time.sleep(0.5)
+            self.playState = True
+            self.ids['btn_getGps'].text = 'Play Start'
+            self.ids['accelerometer'].text = '\n'.join(
+                map(str, accelerometer.acceleration))
+            self.ids['barometer'].text = str(barometer.pressure)+' hPa'
+            self.ids['compass'].text = '\n'.join(map(str, compass.field))
+            self.ids['gravity'].text = '\n'.join(map(str, gravity.gravity))
+            self.ids['infrared'].text = str(irblaster.exists())
+            self.ids['light'].text = str(light.illumination)+' lx'
+            # self.ids['proximity'].text = str(proximity.proximity)
+            self.ids['flash'].text = 'ON'
+            self.ids['temperature'].text = str(temperature.temperature)
+            self.ids['spatial'].text = '\n'.join(
+                map(str, spatialorientation.orientation))
+            self.ids['brightness'].text = str(brightness.current_level())
+            # self.ids['proc'].text = str(processors.status)
+            self.ids['Dp'].text = str(storagepath.get_sdcard_dir())
+            # self.ids['Dp'].text = f"app_dir: {StoragePath.get_application_dir()} \
+            #                         doc_dir: {StoragePath.get_documents_dir_dir()}
+            #                         dow_dir: {StoragePath.get_downloads_dir()}
+            #                         ext_dir: {StoragePath.get_external_storage_dir()}
+            #                         home_dir: {StoragePath.get_home_dir()}
+            #                         pic_dir: {StoragePath.get_pictures_dir()}
+            #                         music_dir: {StoragePath.get_music_dir()}
+            #                         root_dir: {StoragePath.get_root_dir()}
+            #                         sd_dir: {StoragePath.get_sdcard_dir()}"
+        self.ids['bluetooth'].text = str(bluetooth.info)
 
 
-class TestApp(App):
+class BlackApp(App):
 
     def build(self):
-        return Tools()
+        AmainWidget = MainWidget()
+        # request_permissions([Permission.WRITE_SETTINGS])
+        return AmainWidget
 
 
 if __name__ == '__main__':
-    TestApp().run()
+    BlackApp().run()
