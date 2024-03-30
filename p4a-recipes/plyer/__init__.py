6b57b0bf28990cd0b0b5c17d43cdce36194785cb
BlackLee <blacklee1314@outlook.com>
2024-03-30 09:02:35 +0800

    Add plyer master recipe patch.
    Signed-off-by: BlackLee <blacklee1314@outlook.com>

---
+++ p4a-recipes/plyer/__init__.py
@@ -0,0 +1,16 @@
+from pythonforandroid.recipe import PythonRecipe
+from pythonforandroid.toolchain import shprint, current_directory, info
+import sh
+from os.path import join
+
+class PlyerRecipe(PythonRecipe):
+    version = '2.1.0'
+    url = 'https://github.com/kivy/plyer/archive/{version}.zip'
+    name = 'plyer'
+    depends = ['pyjnius']
+    site_packages_name = 'plyer'
+
+    patches = ['master.patch']
+
+
+recipe = PlyerRecipe()
